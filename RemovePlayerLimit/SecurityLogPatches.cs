d5b61f59fb90808fde01d92013adc44b71f39aba
Przebor <przebot@gmail.com>
2021-01-06 13:55:42 +0100

    Mira Security Logger Fix
    Fixes security logger on Mira for players with IDs higher than 9. (bug was caused by the cooldown array which was limited to 10 elements)

---
+++ RemovePlayerLimit/SecurityLogPatches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using SecurityLogger = MAOCFFOEGFE;
+
+namespace CrowdedMod
+{
+    class SecurityLogPatches
+    {
+        [HarmonyPatch(typeof(SecurityLogger), nameof(SecurityLogger.Awake))]
+        public static class SecurityLoggerPatch
+        {
+            public static void Postfix(ref SecurityLogger __instance)
+            {
+                __instance.KJGAJKDMDHJ = new float[127]; // Timers
+            }
+        }
+    }
+}
