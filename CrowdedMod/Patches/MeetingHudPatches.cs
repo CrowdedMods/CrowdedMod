06c74ba1c3e7944b33ccd5e20e19504cfc34268e
Przebor <przebot@gmail.com>
2021-02-14 18:33:10 +0100

    Indentation fix
    Fixed indentation in all files (tabs to spaces)

--- CrowdedMod/Patches/MeetingHudPatches.cs
+++ CrowdedMod/Patches/MeetingHudPatches.cs
@@ -5,164 +5,164 @@ using Hazel;
 
 
 namespace CrowdedMod.Patches {
-	internal static class MeetingHudPatches 
-	{
-		static string lastTimerText;
-		static int currentPage = 0;
-		static int maxPages => (int)Mathf.Ceil(GameData.Instance.AllPlayers.Count / 10f);
+    internal static class MeetingHudPatches 
+    {
+        static string lastTimerText;
+        static int currentPage = 0;
+        static int maxPages => (int)Mathf.Ceil(GameData.Instance.AllPlayers.Count / 10f);
 
-		[HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Update))]
-		static class VoteGuiPatch {
-			public static void Postfix(MeetingHud __instance) {
-				if (Input.GetKeyDown(KeyCode.UpArrow) || Input.mouseScrollDelta.y > 0f)
-					currentPage = Mathf.Clamp(currentPage - 1, 0, maxPages - 1);
-				else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.mouseScrollDelta.y < 0f)
-					currentPage = Mathf.Clamp(currentPage + 1, 0, maxPages - 1);
+        [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Update))]
+        static class VoteGuiPatch {
+            public static void Postfix(MeetingHud __instance) {
+                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.mouseScrollDelta.y > 0f)
+                    currentPage = Mathf.Clamp(currentPage - 1, 0, maxPages - 1);
+                else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.mouseScrollDelta.y < 0f)
+                    currentPage = Mathf.Clamp(currentPage + 1, 0, maxPages - 1);
 
-				if (__instance.TimerText.Text != lastTimerText)
-					__instance.TimerText.Text = (lastTimerText = __instance.TimerText.Text + $" ({currentPage + 1}/{maxPages})");
+                if (__instance.TimerText.Text != lastTimerText)
+                    __instance.TimerText.Text = (lastTimerText = __instance.TimerText.Text + $" ({currentPage + 1}/{maxPages})");
 
-				PlayerVoteArea[] playerButtons = __instance.playerStates.OrderBy(x => x.isDead).ToArray();
-				int i = 0;
-				foreach (PlayerVoteArea button in playerButtons) {
-					if (i >= currentPage * 10 && i < (currentPage + 1) * 10) {
-						button.gameObject.SetActive(true);
+                PlayerVoteArea[] playerButtons = __instance.playerStates.OrderBy(x => x.isDead).ToArray();
+                int i = 0;
+                foreach (PlayerVoteArea button in playerButtons) {
+                    if (i >= currentPage * 10 && i < (currentPage + 1) * 10) {
+                        button.gameObject.SetActive(true);
 
-						int relativeIndex = i % 10;
-						button.transform.localPosition = __instance.VoteOrigin +
-						                                 new Vector3(
-							                                 __instance.VoteButtonOffsets.x * (relativeIndex % 2),
-							                                 __instance.VoteButtonOffsets.y * (relativeIndex / 2), 
-							                                 -1f
-							                             );
-					} else
-						button.gameObject.SetActive(false);
-					i++;
-				}
-			}
-		}
+                        int relativeIndex = i % 10;
+                        button.transform.localPosition = __instance.VoteOrigin +
+                                                         new Vector3(
+                                                             __instance.VoteButtonOffsets.x * (relativeIndex % 2),
+                                                             __instance.VoteButtonOffsets.y * (relativeIndex / 2), 
+                                                             -1f
+                                                         );
+                    } else
+                        button.gameObject.SetActive(false);
+                    i++;
+                }
+            }
+        }
 
-		[HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Method_76))] // CheckForEndVoting()
-		static class MeetingHudCheckForEndVotingPatch {
-			public static bool Prefix(MeetingHud __instance) {
-				if (!__instance.playerStates.All(ps => ps.isDead || ps.didVote)) return false;
-				byte[] self = calculateVotes(__instance.playerStates);
+        [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Method_76))] // CheckForEndVoting()
+        static class MeetingHudCheckForEndVotingPatch {
+            public static bool Prefix(MeetingHud __instance) {
+                if (!__instance.playerStates.All(ps => ps.isDead || ps.didVote)) return false;
+                byte[] self = calculateVotes(__instance.playerStates);
 
-				int maxIdx = indexOfMax(self, out bool tie) - 1;
-				GameData.PlayerInfo exiled = GameData.Instance.GetPlayerById((byte)maxIdx);
-				byte[] states = __instance.playerStates.Select(s => s.GetState()).ToArray();
-				byte[] votes = __instance.playerStates.Select(s => (byte)s.votedFor).ToArray();
-				if (AmongUsClient.Instance.AmHost)
-				{
-					MessageWriter messageWriter = AmongUsClient.Instance.StartRpc(__instance.NetId, 23, SendOption.Reliable);
-					messageWriter.WriteBytesAndSize(states);
-					messageWriter.WriteBytesAndSize(votes); //Added because of the state's 4 bit vote id limit
-					messageWriter.Write(exiled?.PlayerId ?? byte.MaxValue);
-					messageWriter.Write(tie);
-					messageWriter.EndMessage();
-				}
+                int maxIdx = indexOfMax(self, out bool tie) - 1;
+                GameData.PlayerInfo exiled = GameData.Instance.GetPlayerById((byte)maxIdx);
+                byte[] states = __instance.playerStates.Select(s => s.GetState()).ToArray();
+                byte[] votes = __instance.playerStates.Select(s => (byte)s.votedFor).ToArray();
+                if (AmongUsClient.Instance.AmHost)
+                {
+                    MessageWriter messageWriter = AmongUsClient.Instance.StartRpc(__instance.NetId, 23, SendOption.Reliable);
+                    messageWriter.WriteBytesAndSize(states);
+                    messageWriter.WriteBytesAndSize(votes); //Added because of the state's 4 bit vote id limit
+                    messageWriter.Write(exiled?.PlayerId ?? byte.MaxValue);
+                    messageWriter.Write(tie);
+                    messageWriter.EndMessage();
+                }
 
-				MeetingHudHandleRpcPatch.VotingComplete(__instance, states, votes, exiled, tie);
-				return false;
-			}
+                MeetingHudHandleRpcPatch.VotingComplete(__instance, states, votes, exiled, tie);
+                return false;
+            }
 
-			static byte[] calculateVotes(PlayerVoteArea[] states) {
-				byte[] self = new byte[states.Length + 1];
-				foreach (var playerVoteArea in states)
-				{
-					if (playerVoteArea.didVote &&
-					    playerVoteArea.votedFor + 1 >= 0 &&
-					    playerVoteArea.votedFor + 1 < self.Length)
-					{
-						self[playerVoteArea.votedFor + 1]++;
-					}
-				}
-				return self;
-			}
+            static byte[] calculateVotes(PlayerVoteArea[] states) {
+                byte[] self = new byte[states.Length + 1];
+                foreach (var playerVoteArea in states)
+                {
+                    if (playerVoteArea.didVote &&
+                        playerVoteArea.votedFor + 1 >= 0 &&
+                        playerVoteArea.votedFor + 1 < self.Length)
+                    {
+                        self[playerVoteArea.votedFor + 1]++;
+                    }
+                }
+                return self;
+            }
+    
+            static int indexOfMax(byte[] array, out bool tie) {
+                tie = false;
+                int result = -1;
+                int maxNum = int.MinValue;
+                for (int i = 0; i < array.Length; i++) {
+                    int curNum = array[i];
+                    if (curNum > maxNum) {
+                        result = i;
+                        maxNum = curNum;
+                        tie = false;
+                    } else if (curNum == maxNum) {
+                        result = -1;
+                        tie = true;
+                    }
+                }
+                return result;
+            }
+        }
 
-			static int indexOfMax(byte[] array, out bool tie) {
-				tie = false;
-				int result = -1;
-				int maxNum = int.MinValue;
-				for (int i = 0; i < array.Length; i++) {
-					int curNum = array[i];
-					if (curNum > maxNum) {
-						result = i;
-						maxNum = curNum;
-						tie = false;
-					} else if (curNum == maxNum) {
-						result = -1;
-						tie = true;
-					}
-				}
-				return result;
-			}
-		}
+        [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.HandleRpc), typeof(byte), typeof(MessageReader))]
+        static class MeetingHudHandleRpcPatch {
+            static bool Prefix(MeetingHud __instance, [HarmonyArgument(0)] byte callId, [HarmonyArgument(1)] MessageReader reader) {
+                if (callId == 23)
+                {
+                    byte[] states = reader.ReadBytesAndSize();
+                    byte[] votes = reader.ReadBytesAndSize();
+                    GameData.PlayerInfo playerById = GameData.Instance.GetPlayerById(reader.ReadByte());
+                    bool tie = reader.ReadBoolean();
+                    VotingComplete(__instance, states, votes, playerById, tie);
+                    return false;
+                }
+                return true;
+            }
 
-		[HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.HandleRpc), typeof(byte), typeof(MessageReader))]
-		static class MeetingHudHandleRpcPatch {
-			static bool Prefix(MeetingHud __instance, [HarmonyArgument(0)] byte callId, [HarmonyArgument(1)] MessageReader reader) {
-				if (callId == 23)
-				{
-					byte[] states = reader.ReadBytesAndSize();
-					byte[] votes = reader.ReadBytesAndSize();
-					GameData.PlayerInfo playerById = GameData.Instance.GetPlayerById(reader.ReadByte());
-					bool tie = reader.ReadBoolean();
-					VotingComplete(__instance, states, votes, playerById, tie);
-					return false;
-				}
-				return true;
-			}
+            public static void VotingComplete(MeetingHud __instance, byte[] states, byte[] votes, GameData.PlayerInfo exiled, bool tie) {
+                if (__instance.state == MeetingHud.VoteStates.Results) {
+                    return;
+                }
+                __instance.state = MeetingHud.VoteStates.Results;
+                __instance.resultsStartedAt = __instance.discussionTimer;
+                __instance.exiledPlayer = exiled;
+                __instance.wasTie = tie;
+                __instance.SkipVoteButton.gameObject.SetActive(false);
+                __instance.SkippedVoting.gameObject.SetActive(true);
 
-			public static void VotingComplete(MeetingHud __instance, byte[] states, byte[] votes, GameData.PlayerInfo exiled, bool tie) {
-				if (__instance.state == MeetingHud.VoteStates.Results) {
-					return;
-				}
-				__instance.state = MeetingHud.VoteStates.Results;
-				__instance.resultsStartedAt = __instance.discussionTimer;
-				__instance.exiledPlayer = exiled;
-				__instance.wasTie = tie;
-				__instance.SkipVoteButton.gameObject.SetActive(false);
-				__instance.SkippedVoting.gameObject.SetActive(true);
+                PopulateResults(__instance, states, votes);
+                __instance.Method_23(); // SetupProceedButton
+            }
 
-				PopulateResults(__instance, states, votes);
-				__instance.Method_23(); // SetupProceedButton
-			}
+            static void PopulateResults(MeetingHud __instance, byte[] states, byte[] votes) {
+                __instance.TitleText.Text = "Voting Results";
+                int num = 0;
+                for (int i = 0; i < __instance.playerStates.Length; i++) {
+                    PlayerVoteArea playerVoteArea = __instance.playerStates[i];
+                    playerVoteArea.ClearForResults();
+                    int num2 = 0;
+                    for (int j = 0; j < states.Length; j++) {
+                        if ((states[j] & 128) == 0) { //!isDead
+                            GameData.PlayerInfo playerById = GameData.Instance.GetPlayerById((byte)__instance.playerStates[j].TargetPlayerId);
+                            int votedFor = (sbyte)votes[j];
 
-			static void PopulateResults(MeetingHud __instance, byte[] states, byte[] votes) {
-				__instance.TitleText.Text = "Voting Results";
-				int num = 0;
-				for (int i = 0; i < __instance.playerStates.Length; i++) {
-					PlayerVoteArea playerVoteArea = __instance.playerStates[i];
-					playerVoteArea.ClearForResults();
-					int num2 = 0;
-					for (int j = 0; j < states.Length; j++) {
-						if ((states[j] & 128) == 0) { //!isDead
-							GameData.PlayerInfo playerById = GameData.Instance.GetPlayerById((byte)__instance.playerStates[j].TargetPlayerId);
-							int votedFor = (sbyte)votes[j];
+                            SpriteRenderer spriteRenderer = Object.Instantiate(__instance.PlayerVotePrefab);
+                            if (PlayerControl.GameOptions.AnonymousVotes)
+                                PlayerControl.SetPlayerMaterialColors(Palette.DisabledGrey, spriteRenderer);
+                            else
+                                PlayerControl.SetPlayerMaterialColors(playerById.ColorId, spriteRenderer);
+                            spriteRenderer.transform.localScale = Vector3.zero;
 
-							SpriteRenderer spriteRenderer = Object.Instantiate(__instance.PlayerVotePrefab);
-							if (PlayerControl.GameOptions.AnonymousVotes)
-								PlayerControl.SetPlayerMaterialColors(Palette.DisabledGrey, spriteRenderer);
-							else
-								PlayerControl.SetPlayerMaterialColors(playerById.ColorId, spriteRenderer);
-							spriteRenderer.transform.localScale = Vector3.zero;
-
-							if (playerVoteArea.TargetPlayerId == votedFor) {
-								spriteRenderer.transform.SetParent(playerVoteArea.transform);
-								spriteRenderer.transform.localPosition = __instance.CounterOrigin + new Vector3(__instance.CounterOffsets.x * num2, 0f, 0f);
-								__instance.StartCoroutine(Effects.Bloop(num2 * 0.5f, spriteRenderer.transform, 1f, 0.5f));
-								num2++;
-							} else if (i == 0 && votedFor == -1) {
-								spriteRenderer.transform.SetParent(__instance.SkippedVoting.transform);
-								spriteRenderer.transform.localPosition = __instance.CounterOrigin + new Vector3(__instance.CounterOffsets.x * num, 0f, 0f);
-								__instance.StartCoroutine(Effects.Bloop(num * 0.5f, spriteRenderer.transform, 1f, 0.5f));
-								num++;
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+                            if (playerVoteArea.TargetPlayerId == votedFor) {
+                                spriteRenderer.transform.SetParent(playerVoteArea.transform);
+                                spriteRenderer.transform.localPosition = __instance.CounterOrigin + new Vector3(__instance.CounterOffsets.x * num2, 0f, 0f);
+                                __instance.StartCoroutine(Effects.Bloop(num2 * 0.5f, spriteRenderer.transform, 1f, 0.5f));
+                                num2++;
+                            } else if (i == 0 && votedFor == -1) {
+                                spriteRenderer.transform.SetParent(__instance.SkippedVoting.transform);
+                                spriteRenderer.transform.localPosition = __instance.CounterOrigin + new Vector3(__instance.CounterOffsets.x * num, 0f, 0f);
+                                __instance.StartCoroutine(Effects.Bloop(num * 0.5f, spriteRenderer.transform, 1f, 0.5f));
+                                num++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- CrowdedMod/Patches/VitalsPatches.cs
+++ CrowdedMod/Patches/VitalsPatches.cs
@@ -5,65 +5,65 @@ using System.Linq;
 //using HudOverrideTask = LFOILEODBMA;
 
 namespace CrowdedMod.Patches {
-	internal static class VitalsPatches
-	{
-		static int currentPage = 0;
-		const int maxPerPage = 10;
-		static int maxPages => (int)Mathf.Ceil((float)PlayerControl.AllPlayerControls.Count / maxPerPage);
+    internal static class VitalsPatches
+    {
+        static int currentPage = 0;
+        const int maxPerPage = 10;
+        static int maxPages => (int)Mathf.Ceil((float)PlayerControl.AllPlayerControls.Count / maxPerPage);
 
-		[HarmonyPatch(typeof(VitalsMinigame), nameof(VitalsMinigame.Begin))]
-		public static class VitalsGuiPatchBegin
-		{
-			public static void Postfix(VitalsMinigame __instance)
-			{
-				//Fix the name of each player (better multi color handling)
-				VitalsPanel[] vitalsPanels = __instance.vitals;
-				foreach (string color in Palette.ShortColorNames)
-				{
-					VitalsPanel[] colorFiltered = vitalsPanels.Where(panel => panel.Text.Text.Equals(color)).ToArray();
-					if (colorFiltered.Length <= 1)
-						continue;
-					int i = 1;
-					foreach (VitalsPanel panel in colorFiltered)
+        [HarmonyPatch(typeof(VitalsMinigame), nameof(VitalsMinigame.Begin))]
+        public static class VitalsGuiPatchBegin
+        {
+            public static void Postfix(VitalsMinigame __instance)
+            {
+                //Fix the name of each player (better multi color handling)
+                VitalsPanel[] vitalsPanels = __instance.vitals;
+                foreach (string color in Palette.ShortColorNames)
+                {
+                    VitalsPanel[] colorFiltered = vitalsPanels.Where(panel => panel.Text.Text.Equals(color)).ToArray();
+                    if (colorFiltered.Length <= 1)
+                        continue;
+                    int i = 1;
+                    foreach (VitalsPanel panel in colorFiltered)
                     {
-						panel.Text.Text += i;
-						i++;
-					}
-				}
-			}
-		}
-		[HarmonyPatch(typeof(VitalsMinigame), nameof(VitalsMinigame.Update))]
-		public static class VitalsGuiPatchUpdate
-		{
-			public static void Postfix(VitalsMinigame __instance)
+                        panel.Text.Text += i;
+                        i++;
+                    }
+                }
+            }
+        }
+        [HarmonyPatch(typeof(VitalsMinigame), nameof(VitalsMinigame.Update))]
+        public static class VitalsGuiPatchUpdate
+        {
+            public static void Postfix(VitalsMinigame __instance)
             {
                 if (PlayerTask.PlayerHasTaskOfType<HudOverrideTask>(PlayerControl.LocalPlayer))
                     return;
-				//Allow to switch pages
-				if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.mouseScrollDelta.y > 0f)
-					currentPage = Mathf.Clamp(currentPage - 1, 0, maxPages - 1);
-				else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.mouseScrollDelta.y < 0f)
-					currentPage = Mathf.Clamp(currentPage + 1, 0, maxPages - 1);
+                //Allow to switch pages
+                if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.mouseScrollDelta.y > 0f)
+                    currentPage = Mathf.Clamp(currentPage - 1, 0, maxPages - 1);
+                else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.mouseScrollDelta.y < 0f)
+                    currentPage = Mathf.Clamp(currentPage + 1, 0, maxPages - 1);
 
-				//Place dead players at the beginning, disconnected at the end
-				VitalsPanel[] vitalsPanels = __instance.vitals.OrderBy(x => (x.IsDead ? 0 : 1) + (x.IsDiscon ? 2 : 0)).ToArray();//VitalsPanel[] //Sorted by: Dead -> Alive -> dead&disc -> alive&disc
-				int i = 0;
+                //Place dead players at the beginning, disconnected at the end
+                VitalsPanel[] vitalsPanels = __instance.vitals.OrderBy(x => (x.IsDead ? 0 : 1) + (x.IsDiscon ? 2 : 0)).ToArray();//VitalsPanel[] //Sorted by: Dead -> Alive -> dead&disc -> alive&disc
+                int i = 0;
 
-				//Show/hide/move each panel
-				foreach (VitalsPanel panel in vitalsPanels)
-				{
-					if (i >= currentPage * maxPerPage && i < (currentPage + 1) * maxPerPage)
-					{
-						panel.gameObject.SetActive(true);
-						int relativeIndex = i % maxPerPage;
-						// /!\ -2.7f hardcoded, can we get it the same way as MeetingHud.VoteOrigin ?
-						panel.transform.localPosition = new Vector3(-2.7f + 0.6f * relativeIndex, panel.transform.localPosition.y, panel.transform.localPosition.z);
-					}
-					else
-						panel.gameObject.SetActive(false);
-					i++;
-				}
-			}
-		}
-	}
+                //Show/hide/move each panel
+                foreach (VitalsPanel panel in vitalsPanels)
+                {
+                    if (i >= currentPage * maxPerPage && i < (currentPage + 1) * maxPerPage)
+                    {
+                        panel.gameObject.SetActive(true);
+                        int relativeIndex = i % maxPerPage;
+                        // /!\ -2.7f hardcoded, can we get it the same way as MeetingHud.VoteOrigin ?
+                        panel.transform.localPosition = new Vector3(-2.7f + 0.6f * relativeIndex, panel.transform.localPosition.y, panel.transform.localPosition.z);
+                    }
+                    else
+                        panel.gameObject.SetActive(false);
+                    i++;
+                }
+            }
+        }
+    }
 }
