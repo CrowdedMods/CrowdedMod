e7a02f1ea56eefd8c6a7fa22feaa3fb8337f61bc
Galster-dev <46283010+Galster-dev@users.noreply.github.com>
2022-05-25 21:17:27 +0300

    prevent useless updates (optimization) & restore original Z layout instead of vanilla one

--- src/CrowdedMod/Patches/MeetingHudPatches.cs
+++ src/CrowdedMod/Patches/MeetingHudPatches.cs
@@ -12,15 +12,22 @@ namespace CrowdedMod.Patches {
 
         [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Update))]
         public static class VoteGuiPatch {
-            public static void Postfix(MeetingHud __instance) {
+            public static void Postfix(MeetingHud __instance)
+            {
+                var hasChangedPage = true;
                 if (Input.GetKeyDown(KeyCode.UpArrow) || Input.mouseScrollDelta.y > 0f)
                     currentPage = Mathf.Clamp(currentPage - 1, 0, maxPages - 1);
                 else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.mouseScrollDelta.y < 0f)
                     currentPage = Mathf.Clamp(currentPage + 1, 0, maxPages - 1);
+                else
+                    hasChangedPage = false;
+                
 
                 if (__instance.TimerText.text != lastTimerText)
                     __instance.TimerText.text = lastTimerText = __instance.TimerText.text + $" ({currentPage + 1}/{maxPages})";
 
+                if (!hasChangedPage) return;
+                
                 var playerButtons = __instance.playerStates.OrderBy(x => x.AmDead).ToArray();
                 var i = 0;
                 foreach (var button in playerButtons) {
@@ -29,12 +36,13 @@ namespace CrowdedMod.Patches {
 
                         var relativeIndex = i % maxPerPage;
                         var row = relativeIndex / 3;
-                        button.transform.localPosition = __instance.VoteOrigin +
-                                                         new Vector3(
-                                                             __instance.VoteButtonOffsets.x * (relativeIndex % 3),
-                                                             __instance.VoteButtonOffsets.y * row, 
-                                                             -0.9f - row * 0.01f
-                                                         );
+                        var transform = button.transform;
+                        transform.localPosition = __instance.VoteOrigin +
+                                                  new Vector3(
+                                                      __instance.VoteButtonOffsets.x * (relativeIndex % 3),
+                                                      __instance.VoteButtonOffsets.y * row, 
+                                                      transform.localPosition.z
+                                                  );
                     } else
                         button.gameObject.SetActive(false);
                     i++;
