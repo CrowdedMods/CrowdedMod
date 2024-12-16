976de1befaa9eeb64bf98e5a10e58f85f7df2984
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-12-16 09:30:29 +0800

    Fix some bugs regarding findgame gui

--- src/CrowdedMod/Patches/CreateGameOptionsPatches.cs
+++ src/CrowdedMod/Patches/CreateGameOptionsPatches.cs
@@ -82,6 +82,7 @@ internal static class CreateGameOptionsPatches
                 }
             }
 
+            if (__instance.GetTargetOptions().GameMode is GameModes.Normal or GameModes.NormalFools)
             {
                 var secondButton = __instance.ImpostorButtons[1];
                 secondButton.SpriteRenderer.enabled = false;
@@ -134,6 +135,7 @@ internal static class CreateGameOptionsPatches
     {
         public static bool Prefix(CreateOptionsPicker __instance, [HarmonyArgument(0)] IGameOptions opts)
         {
+            if (__instance.mode != SettingsMode.Host) return true;
             if (__instance.CrewArea)
             {
                 __instance.CrewArea.SetCrewSize(opts.MaxPlayers, opts.NumImpostors);
@@ -152,10 +154,25 @@ internal static class CreateGameOptionsPatches
     [HarmonyPatch(typeof(CreateOptionsPicker), nameof(CreateOptionsPicker.UpdateImpostorsButtons))]
     public static class CreateOptionsPicker_UpdateImpostorsButtons
     {
-        public static bool Prefix()
+        public static bool Prefix(CreateOptionsPicker __instance)
         {
+            if (__instance.mode == SettingsMode.Host) return false;
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(CreateOptionsPicker), nameof(CreateOptionsPicker.Refresh))]
+    public static class CreateOptionsPicker_Refresh
+    {
+        public static bool Prefix(CreateOptionsPicker __instance)
+        {
+            IGameOptions targetOptions = __instance.GetTargetOptions();
+            __instance.UpdateImpostorsButtons(targetOptions.NumImpostors);
+            __instance.UpdateMaxPlayersButtons(targetOptions);
+            __instance.UpdateLanguageButton((uint)targetOptions.Keywords);
+            __instance.MapMenu.UpdateMapButtons((int)targetOptions.MapId);
+            __instance.GameModeText.text = DestroyableSingleton<TranslationController>.Instance.GetString(GameModesHelpers.ModeToName[GameOptionsManager.Instance.CurrentGameOptions.GameMode]);
             return false;
         }
     }
 }
-
